7134f23df61ae79ad67571ec2c39cfcd87ea7d54
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2016-12-05 22:25:32 +0700

    add https

--- webbantuin/settings/production.py
+++ webbantuin/settings/production.py
@@ -18,6 +18,7 @@ from django.core.mail import send_mail
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
