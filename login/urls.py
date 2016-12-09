5f8ac925237b03718941a6c922fbb1216a12665d
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2016-12-09 08:54:28 +0700

    remove app ga penting

--- login/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-  	url(r'^$', views.register, name='index')
-]
