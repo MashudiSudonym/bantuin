5956f60da6dc81c6a60d42ea754b1b39c9a4ce23
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2016-12-21 23:07:22 +0700

    tes profil

--- profil/views.py
+++ profil/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def index(request):
-	return render(request, 'profil/profil.html')
+	if request.user.is_authenticated() :
+		context = {
+			"username": request.user.username,
+			"email": request.user.email,
+			"firstname": request.user.first_name,
+			"lastname": request.user.last_name,
+		}
+	else :
+		context = {}
+	return render(request, 'profil/profil.html', context)
