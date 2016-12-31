8f4893b313e3fdfaeef7e5292fbb308dc4ee975d
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2016-12-31 12:56:54 +0700

    menambah link ke profil pada nav about

--- about/views.py
+++ about/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 
 def index(request):
+
+	# gw ga tau caranya biar ga direpeat :( maksudnya biar bisa ambil dari home, biarin gini aja yang penting lebar
 	getusrname = User.objects.filter(username=request.user)
 		
 	context = {
 		"usrname_list": getusrname,
 	}
-	
+
 	return render(request, 'about/about.html', context)
